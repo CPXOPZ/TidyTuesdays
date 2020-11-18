3991fe7b1096603ff9cb73d0be1ccaf2b4604343
CPXOPZ <CPXOPZ@users.noreply.github.com>
2020-11-18 08:35:34 +0800

    W47 RESTING

---
+++ 2020/20201117-W47-Black in Data/20201117-W47-Black in Data.R
@@ -0,0 +1,15 @@
+
+black_in_data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-11-17/black_in_data.csv')
+
+raw_data <- black_in_data
+
+# 只有四列变量
+# 事件时间，标签，目的，网址
+# 时间可用于展示，标签是否有分类作用？目的为文字
+# 摸不着头脑
+
+
+
+# The #TidyTuesday community will be taking a break from our own data this week and asking you to engage with, promote, and support Black people in data.
+
+# We don't have our own dataset this week, but the data below returns the purpose, date, hashtag, and link to each day's events to follow along with.
