9ed32e14f2a732bc3da9dd1818333e95bedcb732
CPXOPZ <CPXOPZ@users.noreply.github.com>
2020-11-24 17:36:58 +0800

    data prepare

---
+++ 2020/20201124-W48-Washington Trails/20201124-W48-Washington Trails.R
@@ -0,0 +1,24 @@
+library(dplyr)
+
+# import data -------------------------------------------------------------
+
+hike_data <- readr::read_rds(url('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-11-24/hike_data.rds'))
+
+hike <- hike_data
+
+skimr::skim(hike)
+
+# length, gain, highpoint, rating  number
+# name, location, feature, description characters
+
+# feature may be interesting.
+
+# clean data --------------------------------------------------------------
+
+# to numbers
+hike <- hike %>%
+  mutate(length = readr::parse_number(length),
+         gain = as.numeric(gain),
+         highpoint = as.numeric(highpoint),
+         rating = as.numeric(rating))
+  
