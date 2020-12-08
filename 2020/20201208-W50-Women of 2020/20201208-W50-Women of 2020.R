7060f56f968ec5fe23a409a4b16f79e319ff4a00
CPXOPZ <CPXOPZ@users.noreply.github.com>
2020-12-08 09:10:23 +0800

    20201208-W50-Women of 2020

---
+++ 2020/20201208-W50-Women of 2020/20201208-W50-Women of 2020.R
@@ -0,0 +1,9 @@
+
+
+women <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-12-08/women.csv')
+
+skimr::skim(women)
+
+# 包含头像，可是试用ggimage
+# 5 category，country可用地图
+
