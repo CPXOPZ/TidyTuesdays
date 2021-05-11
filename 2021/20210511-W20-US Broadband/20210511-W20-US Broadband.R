2cb7da77f60d73a8fa5b7a1401e96211b075273d
CPXOPZ <CPXOPZ@users.noreply.github.com>
2021-05-11 08:42:46 +0800

    开始20周

---
+++ 2021/20210511-W20-US Broadband/20210511-W20-US Broadband.R
@@ -0,0 +1,28 @@
+
+# packages ----------------------------------------------------------------
+library(dplyr)
+
+
+# data import -------------------------------------------------------------
+# tuesdata <- tidytuesdayR::tt_load('2021-05-11')
+# tuesdata <- tidytuesdayR::tt_load(2021, week = 20)
+# 
+# broadband <- tuesdata$broadband
+
+# Or read in the data manually
+
+broadband <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-11/broadband.csv')
+broadband_zip <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-11/broadband_zip.csv')
+
+# data prepare ------------------------------------------------------------
+
+
+# plot --------------------------------------------------------------------
+
+
+
+
+# final plot --------------------------------------------------------------
+## 2021-05-11	US Broadband
+## https://github.com/rfordatascience/tidytuesday/blob/master/data/2021/2021-05-11
+
