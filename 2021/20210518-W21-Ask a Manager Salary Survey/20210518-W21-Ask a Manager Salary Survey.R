9355108c54db965441bacdc9d5a8eecfe5263eb3
CPXOPZ <CPXOPZ@users.noreply.github.com>
2021-05-18 09:29:51 +0800

    开始21周

---
+++ 2021/20210518-W21-Ask a Manager Salary Survey/20210518-W21-Ask a Manager Salary Survey.R
@@ -0,0 +1,26 @@
+
+# packages ----------------------------------------------------------------
+library(dplyr)
+
+# import ------------------------------------------------------------------
+
+# tuesdata <- tidytuesdayR::tt_load('2021-05-18')
+# tuesdata <- tidytuesdayR::tt_load(2021, week = 21)
+# 
+# survey <- tuesdata$survey
+
+# Or read in the data manually
+
+survey <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-18/survey.csv')
+
+# data prepare ------------------------------------------------------------
+
+
+# plot --------------------------------------------------------------------
+
+
+
+# final plot --------------------------------------------------------------
+
+# 20210518-W21-Ask a Manager Salary Survey
+# https://github.com/rfordatascience/tidytuesday/blob/master/data/2021/2021-05-18
