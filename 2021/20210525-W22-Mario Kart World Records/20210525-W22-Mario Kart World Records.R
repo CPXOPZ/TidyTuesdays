e6867c8fa48fcc3a1bd0051b8655197c3824f9e2
CPXOPZ <CPXOPZ@users.noreply.github.com>
2021-05-25 08:45:08 +0800

    开始22周

---
+++ 2021/20210525-W22-Mario Kart World Records/20210525-W22-Mario Kart World Records.R
@@ -0,0 +1,24 @@
+
+# packages ----------------------------------------------------------------
+library(dplyr)
+
+# import ------------------------------------------------------------------
+# tuesdata <- tidytuesdayR::tt_load('2021-05-25')
+# tuesdata <- tidytuesdayR::tt_load(2021, week = 22)
+# 
+# records <- tuesdata$records
+
+# Or read in the data manually
+
+records <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-25/records.csv')
+drivers <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-25/drivers.csv')
+
+# data pre ----------------------------------------------------------------
+
+
+# plot --------------------------------------------------------------------
+
+
+# final plot --------------------------------------------------------------
+# 20210525-W22-Mario Kart World Records
+
---
+++ 2021/20210525-W22-Mario Kart World Records/readme.md
@@ -0,0 +1,4 @@
+https://github.com/rfordatascience/tidytuesday/blob/master/data/2021/2021-05-25
+
+![]()
+
