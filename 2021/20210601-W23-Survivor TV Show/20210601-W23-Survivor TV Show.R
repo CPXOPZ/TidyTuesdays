4c8158ace0bb354905bbe757f62aa51adad9a8a2
CPXOPZ <CPXOPZ@users.noreply.github.com>
2021-06-01 11:16:00 +0800

    开始23周

---
+++ 2021/20210601-W23-Survivor TV Show/20210601-W23-Survivor TV Show.R
@@ -0,0 +1,25 @@
+
+# packages ----------------------------------------------------------------
+library(dplyr)
+
+# data import -------------------------------------------------------------
+# tuesdata <- tidytuesdayR::tt_load('2021-06-01')
+# tuesdata <- tidytuesdayR::tt_load(2021, week = 23)
+# 
+# summary <- tuesdata$summary
+
+# Or read in the data manually
+
+summary <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-06-01/summary.csv')
+
+
+# data prepare ------------------------------------------------------------
+
+
+# plot --------------------------------------------------------------------
+
+
+
+# final plot --------------------------------------------------------------
+# 20210601-W23-Survivor_TV_Show.png
+
---
+++ 2021/20210601-W23-Survivor TV Show/readme.md
@@ -0,0 +1,3 @@
+https://github.com/rfordatascience/tidytuesday/blob/master/data/2021/2021-06-01
+
+![](20210601-W23-Survivor_TV_Show.png)
