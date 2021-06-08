58748398c485c0b6950b44ffd37b92670dbe484a
CPXOPZ <CPXOPZ@users.noreply.github.com>
2021-06-08 08:47:03 +0800

    开始24周

---
+++ 2021/20210608-W24-Great Lakes Fish/20210608-W24-Great Lakes Fish.R
@@ -0,0 +1,24 @@
+
+# packages ----------------------------------------------------------------
+
+
+# data import -------------------------------------------------------------
+# tuesdata <- tidytuesdayR::tt_load('2021-06-08')
+# tuesdata <- tidytuesdayR::tt_load(2021, week = 24)
+# 
+# fishing <- tuesdata$fishing
+
+# Or read in the data manually
+
+fishing <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-06-08/fishing.csv')
+stocked <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-06-08/stocked.csv')
+
+# data pre ----------------------------------------------------------------
+
+
+# plot --------------------------------------------------------------------
+
+
+# final -------------------------------------------------------------------
+
+# 20210608-W24-Great Lakes Fish
