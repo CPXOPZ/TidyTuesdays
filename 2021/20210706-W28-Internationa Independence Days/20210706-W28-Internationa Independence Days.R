62a481b083f9c644167bba3f9ea31eea59219dac
CPXOPZ <CPXOPZ@users.noreply.github.com>
2021-07-17 14:20:47 +0800

    待完成

---
+++ 2021/20210706-W28-Internationa Independence Days/20210706-W28-Internationa Independence Days.R
@@ -0,0 +1,18 @@
+
+# data import -------------------------------------------------------------
+
+# tuesdata <- tidytuesdayR::tt_load('2021-07-06')
+# tuesdata <- tidytuesdayR::tt_load(2021, week = 28)
+# 
+# holidays <- tuesdata$holidays
+
+# Or read in the data manually
+
+holidays <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-06/holidays.csv')
+
+# packages ----------------------------------------------------------------
+
+
+# data prepare ------------------------------------------------------------
+
+
